10229f111a1d619a57b1caafffbe0f7159ab30cf
Mingrui Liu <875139377@qq.com>
2019-06-18 14:41:06 -0400

    Create Homework1.h

---
+++ Homework1.h
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//struct mp3{
+//	int data;
+//	char *name;
+//	char *title;
+//	char *date;
+//	int *time;
+//    struct mp3* prev;
+//    struct mp3* next;
+//};
+//
+//struct mp3 *head = NULL;
+//struct mp3 *last = NULL;
+//struct mp3 *current = NULL;
